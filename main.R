96de9feff66f337a7304cf43a08eee50174d293b
Florian Krull <flo.krull@gmail.com>
2021-02-09 18:13:22 +0100

    init

---
+++ main.R
@@ -0,0 +1,52 @@
+stopifnot("digest" %in% installed.packages())
+
+#-------------------------------------------------------------------------------
+
+decrypt_from_raw <- function(r_enc) {
+  key32 <- digest::digest(readline("password:"), algo="sha256",raw=T)
+  # decrypt
+  aes <- digest::AES(key32, mode="ECB")
+  r_dec <- aes$decrypt(r_enc, raw=TRUE)
+  # unpad data
+  s_dump <- rawToChar(r_dec[r_dec>0])
+  zz <- textConnection(s_dump)
+  source(zz)
+  close(zz)
+}
+
+#-------------------------------------------------------------------------------
+
+encrypt_to_source <- function(objects, password=NULL, key32=NULL, fn_out, on_decrypt=function(){cat("decryption successful\n")}) {
+  if (!is.null(password)) {
+    key32 <- digest::digest(password, algo="sha256", raw=T)
+  }
+  zz <- textConnection("s_dump", "w")
+  dump(objects, file=zz)
+  close(zz)
+  r_dec <- charToRaw(paste(s_dump,collapse="\n"))
+  # pad data
+  r_dec <- c(r_dec,as.raw(rep(0,16-length(r_dec)%%16)))
+  aes <- digest::AES(key32, mode="ECB")
+  r_enc <- aes$encrypt(r_dec)
+  dump("r_enc", file=fn_out)
+  dump("decrypt_from_raw", file=fn_out, append=TRUE)
+  if (!is.null(on_decrypt)) {
+    dump("on_decrypt", file=fn_out, append=TRUE)
+    write(sprintf("decrypt_from_raw(r_enc)"), file=fn_out, append=TRUE)
+    write(sprintf("on_decrypt()"), file=fn_out, append=TRUE)
+    write(sprintf('rm("r_enc","decrypt_from_raw","on_decrypt")'), file=fn_out, append=TRUE)
+  }
+}
+
+#-------------------------------------------------------------------------------
+
+test() {
+  # create test data
+  test_1 <- data.frame(a=c(1,2),b=c("a","b"))
+  test_2 <- function(x) {x*x / 10}
+  # setup pw
+  password <- "lcbc"
+  key32 <- digest::digest(password, algo="sha256", raw=T)
+  # write encryted file
+  encrypt_to_source(c("test_1","test_2"), key=key32, fn_out="/tmp/encsrc.R", on_decrypt=function(){cat("decryption successful!\n")})
+}
